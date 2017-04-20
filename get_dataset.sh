2558fffb1ece5bf557ea0cda182974d486b7a0da
Rik Nijessen <riknijessen@gmail.com>
2017-04-20 14:22:29 +0200

    update generating dataset script

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,7 +1,10 @@
 echo "generating dataset"
 python generate_dataset.py
-cat pairs.csv | sort -u > pairs.csv
+cat pairs.csv | sort -u > pairs2.csv
+rm pairs.csv
+mv pairs2.csv pairs.csv
 echo "getting diffs"
 python download_diffs.py
 echo "removing pairs without diffs"
 python check_diffs.py
+python split_dataset.py
