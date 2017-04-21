cf852f68378d1d07c561923ae521c1e0d91a224d
Rik Nijessen <riknijessen@gmail.com>
2017-04-21 16:57:41 +0200

    fixed formatting

--- check_diffs.py
+++ check_diffs.py
@@ -3,26 +3,27 @@ import os.path
 
 dir = "diffs"
 
+
 def diff_exists(owner, repo, id):
-    file = dir+"/"+owner+"@"+repo+"@"+id+".diff"
+    file = dir + "/" + owner + "@" + repo + "@" + id + ".diff"
     val = os.path.isfile(file)
     if not val:
-	print file
+        print file
     return val
+
 
 f = open("pairs.csv", "r")
 lines = f.read().split("\n")
 f.close()
-#remove header and newline at the end
-lines = lines[1:len(lines)-1]
+# remove header and newline at the end
+lines = lines[1:len(lines) - 1]
 
 f = open("pairs.csv", "w")
 f.write("owner,repo,pr1_id,p2_id\n")
 
 for line in lines:
     owner, repo, pr1, pr2 = line.split(",")
-    if diff_exists(owner,repo,pr1)  and diff_exists(owner, repo, pr2):
-        f.write(owner+","+repo+","+pr1+","+pr2+"\n")
+    if diff_exists(owner, repo, pr1) and diff_exists(owner, repo, pr2):
+        f.write(owner + "," + repo + "," + pr1 + "," + pr2 + "\n")
 
 f.close()
-
--- download_diffs.py
+++ download_diffs.py
@@ -1,14 +1,15 @@
 import os
 from multiprocessing import Pool
 import urllib
+
 dir = "diffs"
 
 
 def download_diff(owner, repo, id):
-    file = dir+"/"+owner+"@"+repo+"@"+id+".diff"
+    file = dir + "/" + owner + "@" + repo + "@" + id + ".diff"
 
     if not os.path.isfile(file):
-        url = "https://www.github.com/"+owner+"/"+repo+"/pull/"+id+".diff"
+        url = "https://www.github.com/" + owner + "/" + repo + "/pull/" + id + ".diff"
         urllib.urlretrieve(url, file)
 
 
@@ -16,7 +17,7 @@ def get_and_save_diff(line):
     # ignore last new line
     if "," in line:
         owner, repo, pr1, pr2 = line.split(",")
-	download_diff(owner, repo, pr1)
+        download_diff(owner, repo, pr1)
         download_diff(owner, repo, pr2)
 
 
@@ -27,6 +28,3 @@ lines = f.read().split("\n")
 f.close()
 
 p.map(get_and_save_diff, lines)
-
-
-
