722a0671959bff95b65673b9060cda1671eda207
Rik Nijessen <riknijessen@gmail.com>
2017-05-05 17:57:20 +0200

    thread safe generator

--- DuplicatePRs/classifiers/preprocessing.py
+++ DuplicatePRs/classifiers/preprocessing.py
@@ -1,7 +1,30 @@
+import threading
+
 import numpy as np
 import math
 from DuplicatePRs.dataset import get_tokenized_data, load_csv
 
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        with self.lock:
+            return self.it.next()
+
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe.
+    """
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+    return g
 
 cached_vectors = {}
 
@@ -36,6 +59,7 @@ def preprocess(texts, embeddings_model, embeddings_size, maxlen):
         results[i] = preprocess_text(text, embeddings_model, maxlen, embeddings_size)
     return results
 
+@threadsafe_generator
 def generator(prs1, prs2, labels, embeddings_model, embeddings_size, maxlen, batch_size):
     while True:
         for i in xrange(0, len(prs1), batch_size):
