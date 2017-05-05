80152a0b7f6cb64c6f3769fcde81e82a6d3f5299
Rik Nijessen <riknijessen@gmail.com>
2017-05-05 17:32:31 +0200

    path fix

--- DuplicatePRs/config.py
+++ DuplicatePRs/config.py
@@ -1,8 +1,9 @@
 import os
 
-training_dataset_file = os.path.abspath(__file__)+"/training_with_negative_samples.csv"
-validation_dataset_file = os.path.abspath(__file__)+"/validation_with_negative_samples.csv"
-test_dataset_file = os.path.abspath(__file__)+"/test_with_negative_samples.csv"
+_current_path = os.path.dirname(os.path.abspath(__file__))
+training_dataset_file = _current_path+"/training_with_negative_samples.csv"
+validation_dataset_file = _current_path+"/validation_with_negative_samples.csv"
+test_dataset_file = _current_path+"/test_with_negative_samples.csv"
 
 embeddings_size = 300
 maxlen = 1500
@@ -10,5 +11,5 @@ maxlen = 1500
 #convolution
 nr_filters = 150
 
-doc2vec_model_directory = os.path.abspath(__file__)+"/doc2vec_models/"
-fasttext_model_directory = os.path.abspath(__file__)+"/fasttext/"
+doc2vec_model_directory = _current_path+"/doc2vec_models/"
+fasttext_model_directory = _current_path+"/fasttext/"
