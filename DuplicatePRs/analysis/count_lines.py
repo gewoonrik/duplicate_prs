38889feaaccdcbb6078d9b8f257b9abacb720c16
Rik Nijessen <riknijessen@gmail.com>
2017-05-06 13:35:32 +0200

    move analysis files to analysis module

---
+++ DuplicatePRs/analysis/count_lines.py
@@ -0,0 +1,31 @@
+from itertools import chain, imap
+# counts the number of lines of the PRs in the duplicate pairs
+from DuplicatePRs.dataset import load_csv, _current_path, get_diff_file
+
+training = load_csv(_current_path+"/training.csv")
+validation = load_csv(_current_path+"/validation")
+test = load_csv(_current_path+"/test.csv")
+all = training + validation + test
+
+def flatmap(f, items):
+    return chain.from_iterable(imap(f, items))
+
+def get_files_by_line(line):
+    owner, repo, pr1, pr2 = line.split(",")
+    return [get_diff_file(owner,repo,pr1), get_diff_file(owner, repo, pr2)]
+
+files = flatmap(get_files_by_line, all)
+
+lines = []
+for file in files:
+    f = open(file, "r")
+    content = f.read()
+    f.close()
+    nrlines = content.count("\n")
+    lines.append(nrlines)
+
+f = open(_current_path+"/analysis/nrlines.txt", "w")
+
+txt = "\n".join(str(x) for x in lines)
+f.write(txt)
+f.close()
