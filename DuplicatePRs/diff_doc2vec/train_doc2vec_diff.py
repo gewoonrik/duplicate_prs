5bd0450f6298d5a8c5e49a03b537f45c3b61a510
Rik Nijessen <riknijessen@gmail.com>
2017-05-06 18:59:00 +0200

    provide total_examples in training

--- DuplicatePRs/diff_doc2vec/train_doc2vec_diff.py
+++ DuplicatePRs/diff_doc2vec/train_doc2vec_diff.py
@@ -26,7 +26,7 @@ model = Doc2Vec(size=config.embeddings_size, dm=0,  window=5, seed=1337, min_cou
 model.build_vocab(documents)
 for epoch in range(10):
     print("epoch "+str(epoch))
-    model.train(documents)
+    model.train(documents, total_examples=len(total))
     model.save(config._current_path+'/doc2vec_models/doc2vec_dbow_epoch'+str(epoch)+'.model')
     model.alpha -= 0.002  # decrease the learning rate
     model.min_alpha = model.alpha  # fix the learning rate, no decay
