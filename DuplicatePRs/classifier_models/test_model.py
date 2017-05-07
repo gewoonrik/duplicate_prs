9fb7761d5dc87478913b9272bcc12b20b60de399
Rik Nijessen <riknijessen@gmail.com>
2017-05-07 17:03:48 +0200

    fix

--- DuplicatePRs/classifier_models/test_model.py
+++ DuplicatePRs/classifier_models/test_model.py
@@ -17,9 +17,10 @@ test_1, test_2, test_labels = get_doc2vec_data(lines)
 
 results = model.predict([test_1, test_2], batch_size=100)
 
+results_rounded = np.round(results).ravel()
 lines_np = np.asarray(lines)
 
-false_negatives = lines_np[results == 0 and test_labels == '1']
-false_positives = lines_np[results == 1 and test_labels == '0']
+false_negatives = lines_np[results_rounded == 0 and test_labels == '1']
+false_positives = lines_np[results_rounded == 1 and test_labels == '0']
 
 print("\n".join(false_negatives))
