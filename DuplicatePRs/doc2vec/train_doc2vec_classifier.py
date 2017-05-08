fe661e4bf721c2c18475362bf2d7a90de48dbf9b
Rik Nijessen <riknijessen@gmail.com>
2017-05-08 15:41:05 +0200

    Revert "also load titles in doc2vec classifier"
    This reverts commit 72fb67d32242a9b74c958f5bd12c29c79b925e47.

--- DuplicatePRs/doc2vec/train_doc2vec_classifier.py
+++ DuplicatePRs/doc2vec/train_doc2vec_classifier.py
@@ -2,40 +2,23 @@ from keras.callbacks import CSVLogger
 from keras.layers import Input, merge, Dense, Dropout
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
-from DuplicatePRs.dataset import load_csv, get_doc2vec_data_diffs, get_doc2vec_data_titles
+from DuplicatePRs.dataset import load_csv, get_doc2vec_data
+from DuplicatePRs import config
 from keras.optimizers import Adam
 from DuplicatePRs import config
 
 print("loading files")
-train = load_csv(config.training_dataset_file)
-validation = load_csv(config.validation_dataset_file)
-test = load_csv(config.test_dataset_file)
-
-tr_1, tr_2, tr_labels = get_doc2vec_data_diffs(train)
-val_1, val_2, val_labels = get_doc2vec_data_diffs(validation)
-test_1, test_2, test_labels = get_doc2vec_data_diffs(test)
-
-tr_titles_1, tr_titles_2, _ = get_doc2vec_data_titles(train)
-val_titles_1, val_titles_2, _ = get_doc2vec_data_titles(validation)
-te_titles_1, te_titles_2, _ = get_doc2vec_data_titles(test)
+tr_1, tr_2, tr_labels = get_doc2vec_data(load_csv(config.training_dataset_file))
+val_1, val_2, val_labels = get_doc2vec_data(load_csv(config.validation_dataset_file))
+test_1, test_2, test_labels = get_doc2vec_data(load_csv(config.test_dataset_file))
 
 pr1 = Input(shape=(300,), dtype='float32', name='pr1_input')
-title1 = Input(shape=(300,), dtype='float32', name='title1_input')
-
 pr2 = Input(shape=(300,), dtype='float32', name='pr2_input')
-title2 = Input(shape=(300,), dtype='float32', name='title2_input')
-
-p1 = merge([pr1, title1])
-p1 = Dense(300, activation='relu')(p1)
-
-p2 = merge([pr2, title2])
-p2 = Dense(300, activation='relu')(p2)
-
-x = merged = merge([p1, pr2], mode='concat')
+x = merged = merge([pr1, pr2], mode='concat')
 x = Dense(600, activation='relu')(x)
 main_output = Dense(1, activation='sigmoid', name='output')(x)
 
-model = Model(input=[pr1, title1, pr2, title2], output=[main_output])
+model = Model(input=[pr1, pr2], output=[main_output])
 
 optimizer = Adam(lr = 0.00011)
 
@@ -47,9 +30,9 @@ checkpoint = ModelCheckpoint(config._current_path+"/classifier_models/doc2vec_cl
 print("train")
 
 
-model.fit([tr_1, tr_titles_1, tr_2, tr_titles_2], tr_labels, batch_size=100, nb_epoch=100,
-          validation_data=([val_1, val_titles_1, val_2, val_titles_2], val_labels), callbacks=[checkpoint])
+model.fit([tr_1, tr_2], tr_labels, batch_size=100, nb_epoch=100,
+          validation_data=([val_1, val_2], val_labels), callbacks=[checkpoint])
 
-results = model.evaluate([test_1, te_titles_1, test_2, te_titles_2], test_labels, batch_size=100)
+results = model.evaluate([test_1, test_2], test_labels, batch_size=100)
 print('Test results: ', results)
 print('On metrics: ', model.metrics_names)
