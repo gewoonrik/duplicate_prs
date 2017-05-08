59f542fb6b5f85c17e086bac654a7e5b3dd8e082
Rik Nijessen <riknijessen@gmail.com>
2017-05-08 15:46:44 +0200

    run doc2vec classifier using only titles

--- DuplicatePRs/doc2vec/train_doc2vec_classifier.py
+++ DuplicatePRs/doc2vec/train_doc2vec_classifier.py
@@ -19,23 +19,23 @@ tr_titles_1, tr_titles_2, _ = get_doc2vec_data_titles(train)
 val_titles_1, val_titles_2, _ = get_doc2vec_data_titles(validation)
 te_titles_1, te_titles_2, _ = get_doc2vec_data_titles(test)
 
-pr1 = Input(shape=(300,), dtype='float32', name='pr1_input')
+#pr1 = Input(shape=(300,), dtype='float32', name='pr1_input')
 title1 = Input(shape=(300,), dtype='float32', name='title1_input')
 
-pr2 = Input(shape=(300,), dtype='float32', name='pr2_input')
+#pr2 = Input(shape=(300,), dtype='float32', name='pr2_input')
 title2 = Input(shape=(300,), dtype='float32', name='title2_input')
 
-p1 = merge([pr1, title1])
-p1 = Dense(300, activation='relu')(p1)
+#p1 = merge([pr1, title1])
+#p1 = Dense(300, activation='relu')(p1)
 
-p2 = merge([pr2, title2])
-p2 = Dense(300, activation='relu')(p2)
+#p2 = merge([pr2, title2])
+@p2 = Dense(300, activation='relu')(p2)
 
-x = merged = merge([p1, pr2], mode='concat')
+x = merged = merge([title1, title2], mode='concat')
 x = Dense(600, activation='relu')(x)
 main_output = Dense(1, activation='sigmoid', name='output')(x)
 
-model = Model(input=[pr1, title1, pr2, title2], output=[main_output])
+model = Model(input=[title1, title2], output=[main_output])
 
 optimizer = Adam(lr = 0.00011)
 
@@ -47,9 +47,9 @@ checkpoint = ModelCheckpoint(config._current_path+"/classifier_models/doc2vec_cl
 print("train")
 
 
-model.fit([tr_1, tr_titles_1, tr_2, tr_titles_2], tr_labels, batch_size=100, nb_epoch=100,
-          validation_data=([val_1, val_titles_1, val_2, val_titles_2], val_labels), callbacks=[checkpoint])
+model.fit([tr_titles_1, tr_titles_2], tr_labels, batch_size=100, nb_epoch=100,
+          validation_data=([val_titles_1, val_titles_2], val_labels), callbacks=[checkpoint])
 
-results = model.evaluate([test_1, te_titles_1, test_2, te_titles_2], test_labels, batch_size=100)
+results = model.evaluate([te_titles_1, te_titles_2], test_labels, batch_size=100)
 print('Test results: ', results)
 print('On metrics: ', model.metrics_names)
