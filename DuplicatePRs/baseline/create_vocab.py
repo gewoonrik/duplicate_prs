90d648744418f28b5a24c3c034b45e29bc0160a3
Rik Nijessen <riknijessen@gmail.com>
2017-05-08 16:34:05 +0200

    create vocal of train and validation

---
+++ DuplicatePRs/baseline/create_vocab.py
@@ -0,0 +1,28 @@
+import pickle
+
+from DuplicatePRs.dataset import load_csv, get_tokenized_data
+from DuplicatePRs import config
+from collections import Counter
+
+min_count = 5
+
+tpr1s, tpr2s, _ = get_tokenized_data(load_csv(config.training_dataset_file))
+valpr1s, valpr2s, _ = get_tokenized_data(load_csv(config.validation_dataset_file))
+
+total = tpr1s+tpr2s+valpr1s+valpr2s
+
+flat = [item for sublist in total for item in sublist]
+
+vocab = Counter(flat)
+
+
+filtered_vocab = {}
+i = 0
+for token, count in vocab.iteritems():
+    if count >= min_count:
+        filtered_vocab[token] = i
+        i += 1
+
+f = open(config._current_path+"/baseline/vocab.py")
+pickle.dump(filtered_vocab,f)
+f.close()
