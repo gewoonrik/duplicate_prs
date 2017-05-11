3aa932e49b4c4432521836c608c168796b20e9de
Rik Nijessen <riknijessen@gmail.com>
2017-05-11 16:18:49 +0200

    stupid partial...

--- DuplicatePRs/classifiers/test_cnn_model.py
+++ DuplicatePRs/classifiers/test_cnn_model.py
@@ -1,5 +1,4 @@
 import argparse
-from functools import partial
 
 from keras.models import load_model
 import keras.backend as K
@@ -19,12 +18,6 @@ def contrastive_loss(y_true, y_pred):
     y_true = -1 * y_true + 1
     return K.mean((1 - y_true) * K.square(y_pred) +  y_true * K.square(K.maximum(margin - y_pred, 0)))
 
-
-def acc(cutoff, y_true, y_pred):
-    k_cutoff = K.ones_like(y_pred) * (0.5-cutoff)
-    return K.mean(K.equal(y_true, K.round(K.clip(y_pred + k_cutoff,0,1))), axis=-1)
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--embeddings_model', default='word2vec')
 parser.add_argument('--model', default='')
@@ -32,6 +25,13 @@ parser.add_argument('--cutoff', default=0.5, type=float)
 
 
 args = parser.parse_args()
+
+def acc(y_true, y_pred):
+    k_cutoff = K.ones_like(y_pred) * (0.5-args.cutoff)
+    return K.mean(K.equal(y_true, K.round(K.clip(y_pred + k_cutoff,0,1))), axis=-1)
+
+
+
 
 
 if(args.embeddings_model == "word2vec"):
@@ -48,8 +48,8 @@ else:
 lines = load_csv(config.test_dataset_file)
 test_gen, test_steps, test_labels = get_preprocessed_generator(config.validation_dataset_file, embeddings_model, config.embeddings_size, None, 5)
 
-acc_cutoff = partial(acc, args.cutoff)
-model = load_model(config._current_path+"/classifier_models/"+args.model,{"contrastive_loss":contrastive_loss, "acc":acc_cutoff})
+
+model = load_model(config._current_path+"/classifier_models/"+args.model,{"contrastive_loss":contrastive_loss, "acc":acc})
 
 
 results = model.evaluate_generator(test_gen, test_steps)
