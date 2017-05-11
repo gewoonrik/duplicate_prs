7856924842bb2ce79d2268a1ec3a47ce027e68dd
Rik Nijessen <riknijessen@gmail.com>
2017-05-11 11:21:03 +0200

    add early stopping

--- DuplicatePRs/doc2vec/train_doc2vec_classifier.py
+++ DuplicatePRs/doc2vec/train_doc2vec_classifier.py
@@ -1,8 +1,8 @@
-from keras.callbacks import CSVLogger
-from keras.layers import Input, merge, Dense, Dropout
+from keras.callbacks import CSVLogger, EarlyStopping
+from keras.layers import Input, merge, Dense
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
-from DuplicatePRs.dataset import load_csv, get_doc2vec_data_diffs, get_doc2vec_data_descriptions
+from DuplicatePRs.dataset import load_csv, get_doc2vec_data_diffs
 from DuplicatePRs import config
 from keras.optimizers import Adam
 from DuplicatePRs import config
@@ -15,35 +15,30 @@ tr_1, tr_2, tr_labels = get_doc2vec_data_diffs(train)
 val_1, val_2, val_labels = get_doc2vec_data_diffs(validation)
 test_1, test_2, test_labels = get_doc2vec_data_diffs(test)
 
-
-tr_titles_1, tr_titles_2, _ = get_doc2vec_data_descriptions(train)
-val_titles_1, val_titles_2, _ = get_doc2vec_data_descriptions(validation)
-te_titles_1, te_titles_2, _ = get_doc2vec_data_descriptions(test)
-
 pr1 = Input(shape=(300,), dtype='float32', name='pr1_input')
-title1 = Input(shape=(300,), dtype='float32', name='title1_input')
 pr2 = Input(shape=(300,), dtype='float32', name='pr2_input')
-title2 = Input(shape=(300,), dtype='float32', name='title2_input')
 
-x = merged = merge([pr1,title1, pr2, title2], mode='concat')
+x = merged = merge([pr1, pr2], mode='concat')
 x = Dense(600, activation='relu')(x)
 main_output = Dense(1, activation='sigmoid', name='output')(x)
 
-model = Model(input=[pr1, title1, pr2, title2], output=[main_output])
+model = Model(input=[pr1, pr2], output=[main_output])
 
 optimizer = Adam(lr = 0.00011)
 
 model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 checkpoint = ModelCheckpoint(config._current_path+"/classifier_models/doc2vec_classifier-{val_acc:5.5f}.hdf5", monitor="val_acc", save_best_only=True)
+early_stopping = EarlyStopping(monitor="val_loss", patience=5)
+csv_logger = CSVLogger(config._current_path+"/classifier_models/doc2vec_classifier.csv")
 
 
 print("train")
 
 
-model.fit([tr_1, tr_titles_1, tr_2, tr_titles_2], tr_labels, batch_size=100, nb_epoch=100,
-          validation_data=([val_1, val_titles_1, val_2, val_titles_2], val_labels), callbacks=[checkpoint])
+model.fit([tr_1, tr_2], tr_labels, batch_size=100, nb_epoch=100,
+          validation_data=([val_1, val_2], val_labels), callbacks=[checkpoint, early_stopping, csv_logger])
 
-results = model.evaluate([test_1, te_titles_1, test_2, te_titles_2], test_labels, batch_size=100)
+results = model.evaluate([test_1, test_2], test_labels, batch_size=100)
 print('Test results: ', results)
 print('On metrics: ', model.metrics_names)
