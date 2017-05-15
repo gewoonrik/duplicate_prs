26131c6c5388a4e20250633379fdd5b253d70d5d
Rik Nijessen <riknijessen@gmail.com>
2017-05-15 22:24:31 +0200

    fixed bugs

--- DuplicatePRs/baseline/train_svm.py
+++ DuplicatePRs/baseline/train_svm.py
@@ -5,9 +5,10 @@ from DuplicatePRs import config
 from DuplicatePRs.dataset import get_tokenized_data_generator, load_csv
 from gensim.corpora import Dictionary
 
-
-tr_gen = get_tokenized_data_generator(load_csv(config.training_dataset_file))
-val_gen = get_tokenized_data_generator(load_csv(config.validation_dataset_file))
+tr_lines = load_csv(config.training_dataset_file)
+val_lines = load_csv(config.validation_dataset_file)
+tr_gen = get_tokenized_data_generator(tr_lines)
+val_gen = get_tokenized_data_generator(val_lines)
 
 print("loading dict")
 dict = Dictionary()
@@ -15,10 +16,10 @@ dict.load(config._current_path+"/baseline/dict")
 
 nr_words = len(dict.token2id)
 
-def dataset_to_bow(generator):
-    matrix = coo_matrix((len(tr_gen), nr_words*2), dtype=int)
+def dataset_to_bow(generator, length):
+    matrix = coo_matrix((length, nr_words*2), dtype=int)
     labels = []
-    for i, (pr1,pr2,label) in enumerate(tr_gen):
+    for i, (pr1,pr2,label) in enumerate(generator):
         pr1_bow = dict.doc2bow(pr1)
         pr2_bow = dict.doc2bow(pr2)
         for (id, count) in pr1_bow:
@@ -28,13 +29,13 @@ def dataset_to_bow(generator):
         labels.append(labels)
     return matrix, labels
 print("creating matrix")
-training_matrix, tr_labels = dataset_to_bow(tr_gen)
+training_matrix, tr_labels = dataset_to_bow(tr_gen, len(tr_lines))
 
 svm = LinearSVC()
 print("fitting ")
 svm.fit(training_matrix, tr_labels)
 
-validation_matrix, val_labels = dataset_to_bow(val_gen)
+validation_matrix, val_labels = dataset_to_bow(val_gen, len(val_lines))
 print("testing")
 acc = svm.score(validation_matrix, val_labels)
 print("accuracy " +str(acc))
