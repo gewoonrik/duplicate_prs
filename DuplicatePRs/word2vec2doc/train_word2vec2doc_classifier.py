3a7bf068a898d48dac4d3a40b1130574780a7694
Rik Nijessen <riknijessen@gmail.com>
2017-05-21 15:18:20 +0200

    nope

--- DuplicatePRs/word2vec2doc/train_word2vec2doc_classifier.py
+++ DuplicatePRs/word2vec2doc/train_word2vec2doc_classifier.py
@@ -18,12 +18,12 @@ pr1 = Input(shape=(300,), dtype='float32', name='pr1_input')
 pr2 = Input(shape=(300,), dtype='float32', name='pr2_input')
 
 x = merged = merge([pr1, pr2], mode='concat')
-#x = Dense(600, activation='relu', name="dense_1")(x)
+x = Dense(600, activation='relu', name="dense_1")(x)
 main_output = Dense(1, activation='sigmoid', name='output')(x)
 
 model = Model(input=[pr1, pr2], output=[main_output])
 
-optimizer = Adam(lr = 0.0001)
+optimizer = Adam(lr = 0.001)
 
 model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -35,7 +35,7 @@ csv_logger = CSVLogger(config._current_path+"/classifier_models/word2vec2doc/tra
 print("train")
 
 
-model.fit([tr_1, tr_2], tr_labels, batch_size=100, nb_epoch=100,
+model.fit([tr_1, tr_2], tr_labels, batch_size=100, nb_epoch=200,
           validation_data=([val_1, val_2], val_labels), callbacks=[checkpoint, early_stopping, csv_logger])
 
 results = model.evaluate([test_1, test_2], test_labels, batch_size=100)
