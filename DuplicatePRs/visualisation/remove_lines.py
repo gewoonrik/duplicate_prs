072b94912fcd3e5894e16bba630945d9324f9acb
Rik Nijessen <riknijessen@gmail.com>
2017-06-05 13:25:59 +0200

    try again

--- DuplicatePRs/visualisation/remove_lines.py
+++ DuplicatePRs/visualisation/remove_lines.py
@@ -21,19 +21,23 @@ def skip_lines(lines):
         after = lines[i+1:]
         yield [x for sublist in (before + after) for x in sublist]
 
-def check_line(doc2vec, model, baseline, other_vector, test):
+def check_line(doc2vec, test):
     (i,test) = test
     vec = doc2vec.infer_vector(test)
-    res = model.predict([np.asarray([vec]), np.asarray([other_vector])])[0][0]
-    return i, res-baseline
+    return i, vec
 
 def get_predictions(doc2vec, model, baseline, lines, other_vector):
-    results = []
-    func = partial(check_line, doc2vec, model, baseline, other_vector)
+    vecs = []
+    vecs2 = []
+    func = partial(check_line, doc2vec)
     p = Pool(16)
-    for i,res in map(func, enumerate(skip_lines(lines))):
-        results[i] = res
-    return results
+    for i,res in p.imap_unordered(func, enumerate(skip_lines(lines))):
+        vecs[i] = res
+        vecs2[i] = other_vector
+    results = model.predict([np.asarray([vecs]), np.asarray([vecs2])])
+
+    print(results)
+    return results - baseline
 
 def test_lines(doc2vec, model, pr1, pr2):
     lines1 = to_lines(pr1)
