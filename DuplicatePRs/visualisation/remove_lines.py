e176c4482af74bc91dbde56052a63d70ba38e603
Rik Nijessen <riknijessen@gmail.com>
2017-06-05 14:46:07 +0200

    fixed small thing

--- DuplicatePRs/visualisation/remove_lines.py
+++ DuplicatePRs/visualisation/remove_lines.py
@@ -22,12 +22,15 @@ def check_line(doc2vec, lines, i):
     vec = doc2vec.infer_vector(test)
     return i, vec
 
-def get_predictions(doc2vec, model, baseline, lines, other_vector):
+def get_predictions(doc2vec, model, baseline, lines, other_vector, first):
     results = []
     func = partial(check_line, doc2vec, lines)
     p = Pool(5)
     for i,res in p.imap_unordered(func, len(lines)):
-        results[i] = model.predict([np.asarray([res]), np.asarray([other_vector])])[0][0] - baseline
+        if first:
+            results[i] = model.predict([np.asarray([res]), np.asarray([other_vector])])[0][0] - baseline
+        else:
+            results[i] = model.predict([np.asarray([other_vector]), np.asarray([res])])[0][0] - baseline
     print(results)
     return results
 
@@ -40,6 +43,6 @@ def test_lines(doc2vec, model, pr1, pr2):
 
     baseline = model.predict([np.asarray([vec1]), np.asarray([vec2])])[0][0]
 
-    predictions1 = get_predictions(doc2vec, model, baseline, lines1, vec2)
-    predictions2 = get_predictions(doc2vec, model, baseline, lines2, vec1)
+    predictions1 = get_predictions(doc2vec, model, baseline, lines1, vec2, True)
+    predictions2 = get_predictions(doc2vec, model, baseline, lines2, vec1, False)
     return np.asarray(predictions1), np.asarray(predictions2), baseline
