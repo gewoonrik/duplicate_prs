25674e7e3b034dd7c4723424b04b94008fe496c4
Rik Nijessen <riknijessen@gmail.com>
2017-06-14 16:58:37 +0200

    train sum grid search

--- DuplicatePRs/baseline/train_svm.py
+++ DuplicatePRs/baseline/train_svm.py
@@ -3,16 +3,14 @@ from __future__ import print_function
 from multiprocessing import Pool
 
 from scipy.sparse import lil_matrix
-import itertools
-
 from sklearn import preprocessing
+from sklearn.grid_search import GridSearchCV
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
 
 from DuplicatePRs import config
 from DuplicatePRs.dataset import load_csv, read_pickled, line_to_tokenized_files
 from gensim.corpora import Dictionary
-from sklearn.svm import SVC
 
 tr_lines = load_csv(config.training_dataset_file)
 val_lines = load_csv(config.validation_dataset_file)
@@ -35,7 +33,7 @@ def line_to_bow(line):
 
 
 def dataset_to_bow(generator, length):
-    matrix = lil_matrix((length, nr_words*2), dtype=int)
+    matrix = lil_matrix((length*2, nr_words*2), dtype=int)
     labels = []
     p = Pool(14)
     print("mapping")
@@ -45,9 +43,12 @@ def dataset_to_bow(generator, length):
         print("creating matrix %s / %s" % (i, length), end='\r')
 
         for (id, count) in pr1_bow:
-            matrix[i,id] = count
+            matrix[2*i,id] = count
+            matrix[2*i+1,id+nr_words] = count
         for (id, count) in pr2_bow:
-            matrix[i,id+nr_words] = count
+            matrix[2*i,id+nr_words] = count
+            matrix[2*i+1,id] = count
+        labels.append(label)
         labels.append(label)
     return matrix, labels
 print("creating matrix")
@@ -58,13 +59,20 @@ scaler = preprocessing.StandardScaler(with_mean=False).fit(training_matrix)
 training_matrix = scaler.transform(training_matrix)
 
 
-svm = LinearSVC(verbose=1, max_iter=10000)
-print("fitting ")
-svm.fit(training_matrix, tr_labels)
+params = [1,0.1,0.01,0.001,0.0001,0.0001,0.00001,0.000001, 0.0000001]
 
-joblib.dump(svm, config._current_path+"/baseline/svm_hard")
+results = []
+for c in params:
+    svm = LinearSVC(verbose=1, max_iter=10000, C=c)
+    print("fitting "+str(c))
+    svm.fit(training_matrix, tr_labels)
 
-validation_matrix, val_labels = dataset_to_bow(val_gen, len(val_lines))
-print("testing")
-acc = svm.score(scaler.transform(validation_matrix), val_labels)
-print("accuracy " +str(acc))
+    joblib.dump(svm, config._current_path+"/baseline/svm_hard-c-"+str(c))
+
+    validation_matrix, val_labels = dataset_to_bow(val_gen, len(val_lines))
+    print("testing")
+    acc = svm.score(scaler.transform(validation_matrix), val_labels)
+    print("accuracy " +str(acc))
+    results.append(acc)
+print(params)
+print(results)
