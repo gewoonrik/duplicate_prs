4218c5e1d168681e0a104c3c5732bea9fe33ab20
Rik Nijessen <riknijessen@gmail.com>
2017-06-15 16:45:04 +0200

    use the hard dataset

--- DuplicatePRs/classifiers/cnn_euclidian.py
+++ DuplicatePRs/classifiers/cnn_euclidian.py
@@ -15,7 +15,7 @@ from DuplicatePRs import config
 
 
 # Training
-batch_size = 50
+batch_size = 5
 epochs = 150
 
 def acc(y_true, y_pred):
@@ -53,7 +53,7 @@ args = parser.parse_args()
 
 if(args.embeddings_model == "word2vec"):
     from gensim.models import Word2Vec
-    w2vec =  Word2Vec.load(config.doc2vec_model_directory+"doc2vec_word2vec_dbow_epoch9.model")
+    w2vec =  Word2Vec.load(config.doc2vec_model_directory+"doc2vec_word2vec_dbow_hard_epoch9.model")
     embeddings_model = w2vec.wv
     # save memory
     del w2vec
@@ -89,15 +89,15 @@ model.compile(loss=contrastive_loss,
 
 print('Train...')
 
-checkpoint = ModelCheckpoint(config._current_path+"/classifier_models/cnn_euclidian_"+args.embeddings_model+"/{val_loss:5.5f}.hdf5", monitor="val_loss", save_best_only=True)
+checkpoint = ModelCheckpoint(config._current_path+"/classifier_models/cnn_euclidian_"+args.embeddings_model+"_hard/{val_loss:5.5f}.hdf5", monitor="val_loss", save_best_only=True)
 early_stopping = EarlyStopping(monitor="val_loss", patience=config.early_stopping_patience)
-csv_logger = CSVLogger(config._current_path+"/classifier_models/cnn_euclidian_"+args.embeddings_model+"/training.csv")
+csv_logger = CSVLogger(config._current_path+"/classifier_models/cnn_euclidian_"+args.embeddings_model+"_hard/training.csv")
 
 model.fit_generator(tr_gen, steps_per_epoch=tr_steps,
                     epochs=epochs,
                     validation_data=val_gen,
                     validation_steps=val_steps,
-                    workers=1, callbacks=[checkpoint, early_stopping, csv_logger])
+                    workers=1, callbacks=[]) #checkpoint, early_stopping, csv_logger])
 #score, acc = model.evaluate_generator(te_gen, steps=te_steps)
 # print('Test score:', score)
 # print('Test accuracy:', acc)
