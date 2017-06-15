dba7113a0eaecebf69cda2285757e5dc030fab6b
Rik Nijessen <riknijessen@gmail.com>
2017-06-15 15:34:59 +0200

    use the hard csvs

--- DuplicatePRs/config.py
+++ DuplicatePRs/config.py
@@ -1,9 +1,9 @@
 import os
 
 _current_path = os.path.dirname(os.path.abspath(__file__))
-training_dataset_file = _current_path+"/training_with_negative_samples.csv"
-validation_dataset_file = _current_path+"/validation_with_negative_samples.csv"
-test_dataset_file = _current_path+"/test_with_negative_samples.csv"
+training_dataset_file = _current_path+"/training_with_negative_samples_hard.csv"
+validation_dataset_file = _current_path+"/validation_with_negative_samples_hard.csv"
+test_dataset_file = _current_path+"/test_with_negative_samples_hard.csv"
 
 embeddings_size = 300
 maxlen = 2000
